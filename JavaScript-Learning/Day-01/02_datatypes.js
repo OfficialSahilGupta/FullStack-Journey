06de02187d1ff21da59023162f4efccda5523177
Sahil Gupta <sahilgupta98122@gmail.com>
2025-08-19 23:38:11 +0530

    Add dataypeBasic.js

---
+++ JavaScript-Learning/Day-01/02_datatypes.js
@@ -0,0 +1,16 @@
+console.log(3 + 3)
+// alert(3 + 3) // we can't use here alert to print this,,,, and we are not using the browser
+
+// premitives data types
+
+// number => 23
+// bigINt => 
+// String => "Copyy"
+// boolean => true/false
+// null => standalone value
+// undefined => value is not defined till now
+// symbol => unique
+
+// object
+let name = "sahil"
+console.log(typeof(name))
