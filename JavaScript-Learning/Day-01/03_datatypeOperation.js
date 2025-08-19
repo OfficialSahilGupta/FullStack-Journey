5bb503b4297663fe24609aaf5253ec5ace7ab7df
Sahil Gupta <sahilgupta98122@gmail.com>
2025-08-20 00:27:40 +0530

    Add DatatypeOperations.js

---
+++ JavaScript-Learning/Day-01/03_datatypeOperation.js
@@ -0,0 +1,48 @@
+let score = 34
+// console.log(score)
+// console.log(typeof(score))
+
+let typeError = "33abc"
+// console.log(typeError)
+
+// let convertError = Number(typeError)
+// console.log(typeof convertError)
+// console.log(convertError) // NaN not a number
+
+// let marks = null
+// let validMarks = Number(marks)
+// console.log(validMarks)
+// console.log(typeof validMarks)
+
+// undefined
+// let midSemMarks = undefined
+// let validMarks = Number(midSemMarks)
+// console.log(validMarks)
+
+// let result = true
+// let validResul = Number(result)
+// console.log(typeof validResul)
+
+let isLoggedIn = true;
+let validLoggedIn = Number(isLoggedIn)
+// console.log(validLoggedIn)
+
+/*
+33 => Number
+"33abc" => NaN
+true => 1; false => 0
+
+*/
+
+// Operations
+let str1 = "samsumg"
+let str2 = " galaxy"
+
+// console.log(str1 + str2)
+
+console.log("1" + 2)
+console.log("1" + 2 + 2)
+console.log(1 + "2")
+console.log(1 + 2 + "3")
+
+
