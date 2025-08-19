3fe3511ea9448184f1a1c39585f9264fd60d46da
Sahil Gupta <sahilgupta98122@gmail.com>
2025-08-19 23:30:25 +0530

    Add basics.js file

---
+++ JavaScript-Learning/Day-01/01_Basics.js
@@ -0,0 +1,12 @@
+const accountId = 123445
+let accountName = "Sahil"
+var accoundAddr = "Chennai"
+village = "avadi"
+let accountState
+
+/*
+const can never be modified
+prefer not to use var 
+because issue in block scope and function
+*/
+console.table([accountId, accountName, accoundAddr, village, accountState])
