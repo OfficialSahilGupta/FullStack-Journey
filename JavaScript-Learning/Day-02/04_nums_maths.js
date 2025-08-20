2fd5615fdd1ad17eded54fc961448af49a5e8de2
Sahil Gupta <sahilgupta98122@gmail.com>
2025-08-20 12:38:14 +0530

    Add nums and maths understanding

---
+++ JavaScript-Learning/Day-02/04_nums_maths.js
@@ -0,0 +1,30 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(240)
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherBalance = 242.432
+// console.log(otherBalance.toPrecision(4));
+const ruppeees = 29421300059300000
+// console.log(ruppeees.toLocaleString());
+// console.log(ruppeees.toLocaleString('en-IN'));
+// Number.MIN_VALUE
+
+// ++++++++=+++++++++++ MATHS  ++++++++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.9));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2,3,4,5,6));
+// console.log(Math.max(4,5,6,3,90));
+
+// console.log(Math.floor((Math.random()*10) + 1));
+
+const min = 10
+const max = 40
+// Math.random() * (max - min + 1)
+// Math.floor(Math.random() * (max - min + 1) + min)
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
