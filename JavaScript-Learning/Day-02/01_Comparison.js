719f4c4993fb1fac6f29a8047ad30c1ee43be2e5
Sahil Gupta <sahilgupta98122@gmail.com>
2025-08-20 09:42:53 +0530

    Add comparisns.js

---
+++ JavaScript-Learning/Day-02/01_Comparison.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 <= 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null <= 0);
+// console.log(null != 0);
+
+// simple things to remmeber for null values are when the null comes along with the mixture of = and <, >, ! then result will be true
+// or else false
+
+// but for undefined gives the result of false only
+// console.log(undefined == 0);
+// console.log(undefined <= 0);
+// console.log(undefined >= 0);
+//console.log(undefined != 0); // this gives the true value
+
+// strict checking for the comparisons
+console.log("3" === 3); //--> gives false result
+
