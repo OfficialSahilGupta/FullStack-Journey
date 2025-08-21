557899eded46aefdf7e7456087fb714ed7eaf8f5
Sahil Gupta <sahilgupta98122@gmail.com>
2025-08-22 01:56:39 +0530

    Add objects and api

---
+++ JavaScript-Learning/Day-03/04_objects.js
@@ -0,0 +1,16 @@
+// de structuring of objectss
+const course = {
+    name: "Js",
+    price: 999,
+    courseInstructor: "Hitesh"
+}
+
+// course.courseInstructor
+const {courseInstructor} = course
+const {courseInstructor: instructor} = course
+
+// console.log(instructor);
+// console.log(courseInstructor);
+
+
+// +++++++++++++++++++++. API +++++++++++++++++++++++++++
